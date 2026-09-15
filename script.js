7018ee4427df7fa1400670d526c66640d8c8d05b
Pooja1703 <129085948+Pooja1703@users.noreply.github.com>
2026-09-15 19:15:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function shortcut(s1, s2) {
-  // your code here
+  const shortcut = (str1, str2) => {
+  if (str1 == "" || str2 == "") {
+    return "";
+  }
+
+  return str1[0] + str2[0];
+};
 }
 
 // Do not change the code below.
